a6fb396d0862bec9f1fb2d41695019a3cb44a176
Michaela Wagner <mickywagner@gmail.com>
2020-03-14 03:20:44 -0600

    Fetch photo array added to context

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,20 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 
 const AppContext = React.createContext()
 
 function AppContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
+
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(json => setAllPhotos(json))
+            .catch(err => console.log(err))
+    }, [])
+
+    console.log(allPhotos)
 
     return(
         <AppContext.Provider value={{allPhotos}}>
