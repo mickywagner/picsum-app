4973242083af343ef3e03f3123f93b55e75930c5
Michaela Wagner <mickywagner@gmail.com>
2020-03-28 04:41:39 -0600

    Total cost calculation and display

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,12 @@
-import React from "react"
+import React, {useContext} from "react"
+import {AppContext} from "../Context"
 
 function CartItem({item}) {
+    const {removeFromCart} = useContext(AppContext)
+
     return(
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
+            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
             <img src={item.url} width="130px" />
             <p>5.99</p>
         </div>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -8,12 +8,14 @@ function Cart() {
     const cartElements = cartItems.map(item => 
         <CartItem key={item.id} item={item} />
     )
+
+    const orderTotal = (cartItems.length * 5.99).toLocaleString("en-us", {style: "currency", currency: "USD"})
     
     return(
         <main className="cart-page">
             <h1>Check out</h1>
             {cartElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {orderTotal} </p>
             <div className="order-button">
                 <button>Submit Order</button>
             </div>
