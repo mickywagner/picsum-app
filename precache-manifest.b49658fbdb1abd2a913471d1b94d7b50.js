d00237160c9b38a127f6caf92efc5917403612ab
Michaela Wagner <mickywagner@gmail.com>
2020-03-28 16:03:58 -0600

    Updates

---
+++ precache-manifest.b49658fbdb1abd2a913471d1b94d7b50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1770b7a7f5b7ee1c3ac290d3f5abb0d0",
+    "url": "/picsum-app/index.html"
+  },
+  {
+    "revision": "05ee33f1952991fa8289",
+    "url": "/picsum-app/static/css/2.0b1d1a89.chunk.css"
+  },
+  {
+    "revision": "064b7e453d8e7785b91b",
+    "url": "/picsum-app/static/css/main.7bf25323.chunk.css"
+  },
+  {
+    "revision": "05ee33f1952991fa8289",
+    "url": "/picsum-app/static/js/2.5c16092f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/picsum-app/static/js/2.5c16092f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "064b7e453d8e7785b91b",
+    "url": "/picsum-app/static/js/main.49a35fd6.chunk.js"
+  },
+  {
+    "revision": "ef015836054cce53f288",
+    "url": "/picsum-app/static/js/runtime-main.7d04bc48.js"
+  },
+  {
+    "revision": "3b5b92922e0d20f0b6a05ca30137488f",
+    "url": "/picsum-app/static/media/remixicon.3b5b9292.eot"
+  },
+  {
+    "revision": "76414508f73cb3fb57073b235c53fa6c",
+    "url": "/picsum-app/static/media/remixicon.76414508.woff2"
+  },
+  {
+    "revision": "c0d34ad379cbf1557481a30be08263ff",
+    "url": "/picsum-app/static/media/remixicon.c0d34ad3.ttf"
+  },
+  {
+    "revision": "d0ac4bc43acfbe23145c4e5730f48c7f",
+    "url": "/picsum-app/static/media/remixicon.d0ac4bc4.svg"
+  },
+  {
+    "revision": "e5b755b03df25eabc565694ec9aad29f",
+    "url": "/picsum-app/static/media/remixicon.e5b755b0.woff"
+  }
+]);
